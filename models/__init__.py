b24f201bf31f4d42274e6e7e080395d20baaecb1
https://Anjeel@bitbucket.org <https://Anjeel@bitbucket.org>
2020-06-15 19:07:12 +0530

    Commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from . import res_partner_project
-from . import project_phases
 from . import project_dashboard
 from . import project_inherit
 from . import project_stages
--- models/mail_channel.py
+++
@@ -1,5 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class MailChannelInherit(models.Model):
-    _inherit = "mail.channel"
