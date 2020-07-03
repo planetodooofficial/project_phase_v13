e3ae07545b859b76b138b5f2d28bea7104084156
https://Anjeel@bitbucket.org <https://Anjeel@bitbucket.org>
2020-07-03 22:27:58 +0530

    Commit

--- models/project_planner.py
+++ models/project_planner.py
@@ -1,10 +1,14 @@
-from odoo import fields, models
+from odoo import fields, models, tools
 
 
 class CalenderPlanner(models.Model):
+    _name = "project.planner"
     _inherit = "project.task"
+    _order = 'name desc, project_id'
+    _auto = False
 
-    project_id = fields.Many2one('project.project', string='Project')
+    project_id = fields.Many2one('project.project', string='Project',
+                                 default=lambda self: self.env.context.get('default_project_id'))
     project_start_date = fields.Date("Project Start Date", related="project_id.project_start_date")
     project_end_date = fields.Date("Project End Date", related="project_id.project_end_date")
     create_plan = fields.Selection([
@@ -12,4 +16,4 @@ class CalenderPlanner(models.Model):
         ('meeting', 'Meeting'),
         ('project', 'Project')
     ], string="Type of Event")
-    calendar_event_id = fields.Many2one('calendar.event', string="Calendar Meeting", ondelete='cascade')
+    calendar_event_id = fields.Many2one('calendar.event', string="Calendar Meeting")
