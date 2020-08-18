0cd7078193458b7323a592a04b89877f8b7f0f0b
https://Anjeel@bitbucket.org <https://Anjeel@bitbucket.org>
2020-08-18 11:31:33 +0530

    Commit

--- project_phases_v13/controllers/announcement_panel.py
+++ project_phases_v13/controllers/announcement_panel.py
@@ -5,18 +5,18 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class AnnouncementPanel(http.Controller):
 
-    @http.route('/project_phase_v13/announcements', auth='user', type='json')
+    @http.route('/project_phases_v13/announcements', auth='user', type='json')
     def announcement_panel(self):
         announce = request.env["announcement.panel"].sudo().search([])
         if not announce:
             return {
                 'html':
-                    request.env.ref('project_phase_v13.announcement_panel_template').render(
+                    request.env.ref('project_phases_v13.announcement_panel_template').render(
                         {'name': 'No announcements'}
             )}
         for ann in announce:
             announcements = {
-                'html': request.env.ref('project_phase_v13.announcement_panel_template').render({
+                'html': request.env.ref('project_phases_v13.announcement_panel_template').render({
                     'name': ann.name
                 })
             }
